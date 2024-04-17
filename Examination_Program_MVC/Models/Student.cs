985b72d018cb29db40809681adb0613ea5b0cd95
Farid Dibirov <farid962@gmail.com>
2024-04-18 03:01:41 +0400

    student controller crud operations added with views

--- Examination_Program_MVC/Models/Student.cs
+++ Examination_Program_MVC/Models/Student.cs
@@ -1,12 +1,24 @@
-﻿namespace Examination_Program_MVC.Models;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Examination_Program_MVC.Models;
 
 public class Student
 {
-	public int Id { get; set; } 
+	public int Id { get; set; }
+
+	[Display(Name = "Student Number")]
 	public int StudentNumber { get; set; }
-	public string FirstName { get; set; } 
+
+	[Display(Name = "First Name")]
+	public string FirstName { get; set; }
+
+	[Display(Name = "Last Name")]
 	public string LastName { get; set; }
+
+	[Display(Name = "Class")]
 	public int Class { get; set; }
 
+	[ValidateNever]
 	public ICollection<Exam> Exams { get; set; }
 }
